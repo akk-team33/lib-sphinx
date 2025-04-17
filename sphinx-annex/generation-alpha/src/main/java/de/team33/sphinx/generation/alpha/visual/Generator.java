d97b948ca90356ff8179cfa011742f4aa9d72662
Andreas Kluge-Kaindl <akk-pg@team33.de>
2025-04-17 11:52:04 +0200

    [generation-alpha] added visual/Generator

---
+++ sphinx-annex/generation-alpha/src/main/java/de/team33/sphinx/generation/alpha/visual/Generator.java
@@ -0,0 +1,51 @@
+package de.team33.sphinx.generation.alpha.visual;
+
+import de.team33.sphinx.generation.alpha.Classes;
+
+import java.awt.*;
+import java.io.IOException;
+import java.lang.System.Logger;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.*;
+import java.util.stream.Stream;
+
+import static java.lang.System.Logger.Level.*;
+
+public class Generator {
+
+    private static final Logger LOG = System.getLogger(Generator.class.getCanonicalName());
+    private static final Path PACKAGE_PATH = Paths.get("src", "main", "java", "de", "team33", "sphinx", "alpha", "visual")
+                                                  .toAbsolutePath()
+                                                  .normalize();
+    public static final OpenOption[] OPEN_OPTIONS = {
+            StandardOpenOption.CREATE_NEW,
+            // StandardOpenOption.CREATE,
+            // StandardOpenOption.TRUNCATE_EXISTING
+    }; // <-- overwrite or not?
+
+    public static void main(final String[] args) {
+        Classes.stream()
+               .filter(c -> !c.equals(Component.class)) // skip anyway!
+               .filter(Component.class::isAssignableFrom)
+               .map(ComponentUtilitySource::new)
+               //.forEach(System.out::println);
+               .forEach(Generator::write);
+    }
+
+    private static void write(final ComponentUtilitySource source) {
+        final String fileName = source.getName() + ".java";
+        final Path filePath = PACKAGE_PATH.resolve(fileName);
+        try {
+            Files.write(filePath, source.toString().getBytes(StandardCharsets.UTF_8), OPEN_OPTIONS);
+        } catch (final IOException e) {
+            LOG.log(INFO, () -> "failed (already existing?): " + fileName + " (" + filePath + ")");
+        }
+    }
+
+    private static Stream<Class<?>> ancestors(final Class<?> subject) {
+        return (null == subject)
+                ? Stream.empty()
+                : Stream.concat(ancestors(subject.getSuperclass()),
+                                Stream.of(subject));
+    }
+}
---
+++ sphinx-annex/generation-alpha/src/main/java/de/team33/sphinx/generation/alpha/visual/SetupInterfaceSource.java
@@ -0,0 +1,65 @@
+package de.team33.sphinx.generation.alpha.visual;
+
+import java.awt.*;
+import java.lang.reflect.Modifier;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static java.util.Comparator.comparing;
+
+public class SetupInterfaceSource {
+
+    private static final String FORMAT = //
+            "%n%n" +
+            "    /**%n" +
+            "     * Utility interface to set up a target instance of {@link %1$s}.%n" +
+            "     * %n" +
+            "     * @param <T> The final type of the target instance, at least {@link %1$s}.%n" +
+            "     * @param <S> The final type of the Setup implementation.%n" +
+            "     */%n" +
+            "    @SuppressWarnings(\"ClassNameSameAsAncestorName\")%n" +
+            "    @FunctionalInterface%n" +
+            "    public interface Setup<T extends %1$s, S extends Setup<T, S>> extends %2$ss.Setup<T, S> {%3$s%n" +
+            "    }";
+
+    private final String target;
+    private final String ancestor;
+    private final String methods;
+    private final Set<Class<?>> dependencies;
+
+    public SetupInterfaceSource(final Class<?> componentClass) {
+        this.dependencies = new HashSet<>(Arrays.asList(componentClass, componentClass.getSuperclass()));
+        this.target = componentClass.getSimpleName();
+        this.ancestor = componentClass.getSuperclass().getSimpleName();
+        this.methods = Stream.of(componentClass.getMethods())
+                             .filter(method -> !Modifier.isStatic(method.getModifiers()))
+                             .filter(method -> method.getDeclaringClass().equals(componentClass))
+                             .filter(method -> Stream.of("set", "add", "remove")
+                                                     .anyMatch(prefix -> method.getName().startsWith(prefix)))
+                             .filter(method -> !method.getName().endsWith("Listener"))
+                             .filter(method -> !method.getName().endsWith("Notify"))
+                             .map(SetupMethodSource::new)
+                             .peek(source -> dependencies.addAll(source.getDependencies()))
+                             .sorted(comparing(SetupMethodSource::getSignature))
+                             .map(SetupMethodSource::toString)
+                             .collect(Collectors.joining());
+    }
+
+    public final Set<Class<?>> getDependencies() {
+        return dependencies;
+    }
+
+    @Override
+    public final String toString() {
+        return String.format(FORMAT, target, ancestor, methods);
+    }
+
+    public static void main(final String[] args) {
+        final SetupInterfaceSource source = new SetupInterfaceSource(Container.class);
+        source.getDependencies().forEach(System.out::println);
+        System.out.println(source);
+    }
+}
