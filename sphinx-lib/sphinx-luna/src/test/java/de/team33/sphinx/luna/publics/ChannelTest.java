9152c0d6ee82ea8f6943a03159a356989aa61490
Andreas Kluge-Kaindl <akk-pg@team33.de>
2025-04-19 17:39:52 +0200

    [sphinx-alpha] added test

---
+++ sphinx-lib/sphinx-luna/src/test/java/de/team33/sphinx/luna/publics/ChannelTest.java
@@ -0,0 +1,60 @@
+package de.team33.sphinx.luna.publics;
+
+import de.team33.sphinx.luna.Channel;
+import org.junit.jupiter.api.Test;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ComponentEvent;
+import java.awt.event.ContainerEvent;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.function.Supplier;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertSame;
+
+class ChannelTest {
+
+    @Test
+    final void componentAdded() {
+        final Given<Container, ContainerEvent> given = new Given<>(new JPanel());
+        final JLabel label = new JLabel();
+
+        Channel.COMPONENT_ADDED.add(given.component(), given::onMessage);
+        given.component().add(label);
+
+        assertEquals(1, given.received().size());
+        final ContainerEvent received = given.received().get(0);
+        assertSame(given.component(), received.getComponent());
+        assertSame(given.component(), received.getContainer());
+        assertSame(label, received.getChild());
+    }
+
+    static class Given<C extends Component, M extends ComponentEvent> {
+
+        private final C component;
+        private final List<M> received = new LinkedList<>();
+
+        Given(final Supplier<C> newComponent) {
+            this(newComponent.get());
+        }
+
+        Given(final C component) {
+            this.component = component;
+        }
+
+        final void onMessage(final M message) {
+            received.add(message);
+        }
+
+        final C component() {
+            return component;
+        }
+
+        final List<M> received() {
+            return Collections.unmodifiableList(received);
+        }
+    }
+}
