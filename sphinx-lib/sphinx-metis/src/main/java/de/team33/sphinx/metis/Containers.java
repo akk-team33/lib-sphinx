38d28c55675ab0cbe05985a66dbb41a764544166
Andreas Kluge-Kaindl <akk-pg@team33.de>
2025-08-23 21:55:53 +0200

    [sphinx-metis] Refined Containers

--- sphinx-lib/sphinx-metis/src/main/java/de/team33/sphinx/metis/Containers.java
+++ sphinx-lib/sphinx-metis/src/main/java/de/team33/sphinx/metis/Containers.java
@@ -23,150 +23,150 @@ public final class Containers {
 
     /**
      * Returns a new {@link Builder} for target instances as supplied by the given {@link Supplier}.
-     * 
-     * @param <T> The final type of the target instances, at least {@link Container}.
+     *
+     * @param <C> The final type of the target instances, at least {@link Container}.
      */
-    public static <T extends Container> Builder<T> builder(final Supplier<T> newTarget) {
+    public static <C extends Container> Builder<C> builder(final Supplier<C> newTarget) {
         return new Builder<>(newTarget, Builder.class);
     }
 
     /**
      * Returns a new {@link Charger} for a given target instance.
-     * 
-     * @param <T> The final type of the target instance, at least {@link Container}.
+     *
+     * @param <C> The final type of the target instance, at least {@link Container}.
      */
-    public static <T extends Container> Charger<T> charger(final T target) {
+    public static <C extends Container> Charger<C> charger(final C target) {
         return new Charger<>(target, Charger.class);
     }
 
     /**
-     * Builder implementation to build target instances of {@link Container}.
-     * 
-     * @param <T> The final type of the target instances, at least {@link Container}.
-     */
-    public static final class Builder<T extends Container>
-            extends LateBuilder<T, Builder<T>> implements Setup<T, Builder<T>> {
-
-        @SuppressWarnings({"rawtypes", "unchecked"})
-        private Builder(final Supplier<T> newResult, final Class builderClass) {
-            super(newResult, builderClass);
-        }
-    }
-
-    /**
-     * Charger implementation to charge target instances of {@link Container}.
-     * 
-     * @param <T> The final type of the target instance, at least {@link Container}.
-     */
-    public static final class Charger<T extends Container>
-            extends de.team33.patterns.building.elara.Charger<T, Charger<T>>
-            implements Setup<T, Charger<T>> {
-
-        @SuppressWarnings({"rawtypes", "unchecked"})
-        private Charger(final T target, final Class chargerClass) {
-            super(target, chargerClass);
-        }
-    }
-
-    /**
      * Utility interface to set up a target instance of {@link Container}.
-     * 
-     * @param <T> The final type of the target instance, at least {@link Container}.
+     *
+     * @param <C> The final type of the target instance, at least {@link Container}.
      * @param <S> The final type of the Setup implementation.
      */
     @SuppressWarnings("ClassNameSameAsAncestorName")
     @FunctionalInterface
-    public interface Setup<T extends Container, S extends Setup<T, S>> extends Components.Setup<T, S> {
+    public interface Setup<C extends Container, S extends Setup<C, S>> extends Components.Setup<C, S> {
 
         /**
          * @see Container#add(Component)
          */
-        default S add(final Component arg0) {
-            return setup(result -> result.add(arg0));
+        default S add(final Component component) {
+            return setup(result -> result.add(component));
         }
 
         /**
          * @see Container#add(Component, Object)
          */
-        default S add(final Component arg0, final Object arg1) {
-            return setup(result -> result.add(arg0, arg1));
+        default S add(final Component component, final Object constraints) {
+            return setup(result -> result.add(component, constraints));
         }
 
         /**
          * @see Container#add(Component, Object, int)
          */
-        default S add(final Component arg0, final Object arg1, final int arg2) {
-            return setup(result -> result.add(arg0, arg1, arg2));
+        default S add(final Component component, final Object constraints, final int index) {
+            return setup(result -> result.add(component, constraints, index));
         }
 
         /**
          * @see Container#add(Component, int)
          */
-        default S add(final Component arg0, final int arg1) {
-            return setup(result -> result.add(arg0, arg1));
+        default S add(final Component component, final int index) {
+            return setup(result -> result.add(component, index));
         }
 
         /**
          * @see Container#add(String, Component)
          */
-        default S add(final String arg0, final Component arg1) {
-            return setup(result -> result.add(arg0, arg1));
+        default S add(final String name, final Component component) {
+            return setup(result -> result.add(name, component));
         }
 
         /**
          * @see Container#remove(Component)
          */
-        default S remove(final Component arg0) {
-            return setup(result -> result.remove(arg0));
+        default S remove(final Component component) {
+            return setup(result -> result.remove(component));
         }
 
         /**
          * @see Container#remove(int)
          */
-        default S remove(final int arg0) {
-            return setup(result -> result.remove(arg0));
+        default S remove(final int index) {
+            return setup(result -> result.remove(index));
         }
 
         /**
          * @see Container#removeAll()
          */
         default S removeAll() {
-            return setup(result -> result.removeAll());
+            return setup(Container::removeAll);
         }
 
         /**
          * @see Container#setComponentZOrder(Component, int)
          */
-        default S setComponentZOrder(final Component arg0, final int arg1) {
-            return setup(result -> result.setComponentZOrder(arg0, arg1));
+        default S setComponentZOrder(final Component component, final int index) {
+            return setup(result -> result.setComponentZOrder(component, index));
         }
 
         /**
          * @see Container#setFocusCycleRoot(boolean)
          */
-        default S setFocusCycleRoot(final boolean arg0) {
-            return setup(result -> result.setFocusCycleRoot(arg0));
+        default S setFocusCycleRoot(final boolean focusCycleRoot) {
+            return setup(result -> result.setFocusCycleRoot(focusCycleRoot));
         }
 
         /**
          * @see Container#setFocusTraversalPolicy(FocusTraversalPolicy)
          */
-        default S setFocusTraversalPolicy(final FocusTraversalPolicy arg0) {
-            return setup(result -> result.setFocusTraversalPolicy(arg0));
+        default S setFocusTraversalPolicy(final FocusTraversalPolicy policy) {
+            return setup(result -> result.setFocusTraversalPolicy(policy));
         }
 
         /**
          * @see Container#setFocusTraversalPolicyProvider(boolean)
          */
-        default S setFocusTraversalPolicyProvider(final boolean arg0) {
-            return setup(result -> result.setFocusTraversalPolicyProvider(arg0));
+        default S setFocusTraversalPolicyProvider(final boolean provider) {
+            return setup(result -> result.setFocusTraversalPolicyProvider(provider));
         }
 
         /**
          * @see Container#setLayout(LayoutManager)
          */
-        default S setLayout(final LayoutManager arg0) {
-            return setup(result -> result.setLayout(arg0));
+        default S setLayout(final LayoutManager manager) {
+            return setup(result -> result.setLayout(manager));
+        }
+    }
+
+    /**
+     * Builder implementation to build target instances of {@link Container}.
+     *
+     * @param <C> The final type of the target instances, at least {@link Container}.
+     */
+    public static final class Builder<C extends Container>
+            extends LateBuilder<C, Builder<C>> implements Setup<C, Builder<C>> {
+
+        @SuppressWarnings("unchecked")
+        private Builder(final Supplier<C> newResult, final Class builderClass) {
+            super(newResult, builderClass);
+        }
+    }
+
+    /**
+     * Charger implementation to charge target instances of {@link Container}.
+     *
+     * @param <C> The final type of the target instance, at least {@link Container}.
+     */
+    public static final class Charger<C extends Container>
+            extends de.team33.patterns.building.elara.Charger<C, Charger<C>>
+            implements Setup<C, Charger<C>> {
+
+        @SuppressWarnings("unchecked")
+        private Charger(final C target, final Class chargerClass) {
+            super(target, chargerClass);
         }
     }
 }
