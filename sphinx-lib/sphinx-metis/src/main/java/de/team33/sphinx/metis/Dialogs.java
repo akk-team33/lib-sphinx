77fd1f7488a1b0fabe5b222de82560a74499798f
Andreas Kluge-Kaindl <akk-pg@team33.de>
2025-08-23 22:46:34 +0200

    [sphinx-metis] Refined Dialogs

--- sphinx-lib/sphinx-metis/src/main/java/de/team33/sphinx/metis/Dialogs.java
+++ sphinx-lib/sphinx-metis/src/main/java/de/team33/sphinx/metis/Dialogs.java
@@ -17,94 +17,135 @@ public final class Dialogs {
 
     /**
      * Returns a new {@link Builder} for target instances as supplied by the given {@link Supplier}.
-     * 
-     * @param <T> The final type of the target instances, at least {@link Dialog}.
+     *
+     * @param <D> The final type of the target instances, at least {@link Dialog}.
      */
-    public static <T extends Dialog> Builder<T> builder(final Supplier<T> newTarget) {
+    public static <D extends Dialog> Builder<D> builder(final Supplier<D> newTarget) {
         return new Builder<>(newTarget, Builder.class);
     }
 
     /**
-     * Returns a new {@link Charger} for a given target instance.
-     * 
-     * @param <T> The final type of the target instance, at least {@link Dialog}.
+     * @see #builder(Supplier)
+     * @see Dialog#Dialog(Window)
      */
-    public static <T extends Dialog> Charger<T> charger(final T target) {
+    public static Builder<Dialog> builder(final Window owner) {
+        return new Builder<>(() -> new Dialog(owner), Builder.class);
+    }
+
+    /**
+     * @see #builder(Supplier)
+     * @see Dialog#Dialog(Window, String)
+     */
+    public static Builder<Dialog> builder(final Window owner, final String title) {
+        return new Builder<>(() -> new Dialog(owner, title), Builder.class);
+    }
+
+    /**
+     * @see #builder(Supplier)
+     * @see Dialog#Dialog(Window, ModalityType)
+     */
+    public static Builder<Dialog> builder(final Window owner, final ModalityType type) {
+        return new Builder<>(() -> new Dialog(owner, type), Builder.class);
+    }
+
+    /**
+     * @see #builder(Supplier)
+     * @see Dialog#Dialog(Window, String, ModalityType)
+     */
+    public static Builder<Dialog> builder(final Window owner, final String title, final ModalityType type) {
+        return new Builder<>(() -> new Dialog(owner, title, type), Builder.class);
+    }
+
+    /**
+     * @see #builder(Supplier)
+     * @see Dialog#Dialog(Window, String, ModalityType, GraphicsConfiguration)
+     */
+    public static Builder<Dialog> builder(final Window owner, final String title,
+                                          final ModalityType type, final GraphicsConfiguration gc) {
+        return new Builder<>(() -> new Dialog(owner, title, type, gc), Builder.class);
+    }
+
+    /**
+     * Returns a new {@link Charger} for a given target instance.
+     *
+     * @param <D> The final type of the target instance, at least {@link Dialog}.
+     */
+    public static <D extends Dialog> Charger<D> charger(final D target) {
         return new Charger<>(target, Charger.class);
     }
 
     /**
-     * Builder implementation to build target instances of {@link Dialog}.
-     * 
-     * @param <T> The final type of the target instances, at least {@link Dialog}.
+     * Utility interface to set up a target instance of {@link Dialog}.
+     *
+     * @param <D> The final type of the target instance, at least {@link Dialog}.
+     * @param <S> The final type of the Setup implementation.
      */
-    public static final class Builder<T extends Dialog>
-            extends LateBuilder<T, Builder<T>> implements Setup<T, Builder<T>> {
+    @SuppressWarnings("ClassNameSameAsAncestorName")
+    @FunctionalInterface
+    public interface Setup<D extends Dialog, S extends Setup<D, S>> extends Windows.Setup<D, S> {
 
-        @SuppressWarnings({"rawtypes", "unchecked"})
-        private Builder(final Supplier<T> newResult, final Class builderClass) {
+        /**
+         * @see Dialog#setModal(boolean)
+         */
+        default S setModal(final boolean modal) {
+            return setup(result -> result.setModal(modal));
+        }
+
+        /**
+         * @see Dialog#setModalityType(ModalityType)
+         */
+        default S setModalityType(final ModalityType type) {
+            return setup(result -> result.setModalityType(type));
+        }
+
+        /**
+         * @see Dialog#setResizable(boolean)
+         */
+        default S setResizable(final boolean resizable) {
+            return setup(result -> result.setResizable(resizable));
+        }
+
+        /**
+         * @see Dialog#setTitle(String)
+         */
+        default S setTitle(final String title) {
+            return setup(result -> result.setTitle(title));
+        }
+
+        /**
+         * @see Dialog#setUndecorated(boolean)
+         */
+        default S setUndecorated(final boolean undecorated) {
+            return setup(result -> result.setUndecorated(undecorated));
+        }
+    }
+
+    /**
+     * Builder implementation to build target instances of {@link Dialog}.
+     *
+     * @param <D> The final type of the target instances, at least {@link Dialog}.
+     */
+    public static final class Builder<D extends Dialog>
+            extends LateBuilder<D, Builder<D>> implements Setup<D, Builder<D>> {
+
+        @SuppressWarnings("unchecked")
+        private Builder(final Supplier<D> newResult, final Class builderClass) {
             super(newResult, builderClass);
         }
     }
 
     /**
      * Charger implementation to charge target instances of {@link Dialog}.
-     * 
-     * @param <T> The final type of the target instance, at least {@link Dialog}.
+     *
+     * @param <D> The final type of the target instance, at least {@link Dialog}.
      */
-    public static final class Charger<T extends Dialog>
-            extends de.team33.patterns.building.elara.Charger<T, Charger<T>>
-            implements Setup<T, Charger<T>> {
+    public static final class Charger<D extends Dialog>
+            extends de.team33.patterns.building.elara.Charger<D, Charger<D>>
+            implements Setup<D, Charger<D>> {
 
-        @SuppressWarnings({"rawtypes", "unchecked"})
-        private Charger(final T target, final Class chargerClass) {
+        @SuppressWarnings("unchecked")
+        private Charger(final D target, final Class chargerClass) {
             super(target, chargerClass);
-        }
-    }
-
-    /**
-     * Utility interface to set up a target instance of {@link Dialog}.
-     * 
-     * @param <T> The final type of the target instance, at least {@link Dialog}.
-     * @param <S> The final type of the Setup implementation.
-     */
-    @SuppressWarnings("ClassNameSameAsAncestorName")
-    @FunctionalInterface
-    public interface Setup<T extends Dialog, S extends Setup<T, S>> extends Windows.Setup<T, S> {
-
-        /**
-         * @see Dialog#setModal(boolean)
-         */
-        default S setModal(final boolean arg0) {
-            return setup(result -> result.setModal(arg0));
-        }
-
-        /**
-         * @see Dialog#setModalityType(ModalityType)
-         */
-        default S setModalityType(final ModalityType arg0) {
-            return setup(result -> result.setModalityType(arg0));
-        }
-
-        /**
-         * @see Dialog#setResizable(boolean)
-         */
-        default S setResizable(final boolean arg0) {
-            return setup(result -> result.setResizable(arg0));
-        }
-
-        /**
-         * @see Dialog#setTitle(String)
-         */
-        default S setTitle(final String arg0) {
-            return setup(result -> result.setTitle(arg0));
-        }
-
-        /**
-         * @see Dialog#setUndecorated(boolean)
-         */
-        default S setUndecorated(final boolean arg0) {
-            return setup(result -> result.setUndecorated(arg0));
         }
     }
 }
